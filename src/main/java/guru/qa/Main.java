7dfdeb57defcb1f31da64944890f61751c25005b
Sergey Ermolaev <bertzorbrist@gmail.com>
2024-02-22 09:27:19 +0300

    добавил новый class Main

--- src/main/java/guru/qa/Main.java
+++ src/main/java/guru/qa/Main.java
@@ -1,14 +1,14 @@
 package guru.qa;
 
-public class MyProgram {
+public class Main {
 
-    public static void main(){
-        //Примитивные
+    public static void main(String[] args) {
+        //Примитивные типы данных
         //Целочисленные значения
-        byte aByte = 0; //8b  -128 +127
-        short aShort = 0; //16 -32768 +32767
-        int aInt = 0; //32b -14 mln (-2^32) ... (+2^32 - 1)
-        long aLong = 0; //64b  .... (-2^64) ... (+2^64 - 1)
+        byte aByte = 0; //8b  -128 +127 (-2^7) ... (+2^7 - 1)
+        short aShort = 0; //16 -32768 +32767  (-2^15) ... (+2^15 - 1)
+        int aInt = 0; //32b -14 mln (-2^31) ... (+2^31 - 1)
+        long aLong = 0; //64b  .... (-2^63) ... (+2^63 - 1)
 
         //Типы с плавающей точкой
         float aFloat = 0.0F;
@@ -24,9 +24,7 @@ public class MyProgram {
         String toBePrint = "Hello world!";
 
 
-        System.out.println(toBePrint);
-
-
+        System.out.println("Hello world!");
 
 
     }
