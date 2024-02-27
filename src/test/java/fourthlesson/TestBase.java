e8f6a29a46926d9dcb1081eb0233f0415f1628fb
Sergey Ermolaev <157000239+Zorbrist99@users.noreply.github.com>
2024-02-27 21:23:56 +0300

    Update TestBase.java

--- src/test/java/fourthlesson/TestBase.java
+++ src/test/java/fourthlesson/TestBase.java
@@ -15,8 +15,6 @@ public class TestBase {
         Configuration.baseUrl = "https://demoqa.com/";
         Configuration.browserSize = "1920x1080";
         Configuration.pageLoadStrategy = "eager";
-        Configuration.holdBrowserOpen = true;
-
 
 //        Faker faker = new Faker(new Locale("ru"));
 
