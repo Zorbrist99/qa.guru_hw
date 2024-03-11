b9193756fa9c8fcd0e65a21bc222b51bf866bd4b
Sergey Ermolaev <bertzorbrist@gmail.com>
2024-03-11 17:01:13 +0300

    добавил тест для скачивания файлов по href

---
+++ src/test/java/eighthlesson/SearchFail.java
@@ -0,0 +1,35 @@
+package eighthlesson;
+
+import org.apache.commons.io.FileUtils;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class SearchFail {
+
+
+    @Test
+    void downloadedFail() throws Exception {
+        open("https://github.com/junit-team/junit5/blob/main/README.md");
+        File file = $(".react-blob-header-edit-and-raw-actions  [href*='main/README.md']").download();
+
+        // Развёрнутая версия чтения файла в строку
+        try (InputStream is = new FileInputStream(file);) {
+            byte[] fileContent = is.readAllBytes();
+            String strContent = new String(fileContent, StandardCharsets.UTF_8);
+//            Проверка совпадения текста в файле и того, что нам нужно
+//            Assertions.assertTrue(strContent.contains("Contributions to JUnit 5 are both welcomed and appreciated."));
+            System.out.println(strContent.contains("Contributions to JUnit 5 are both welcomed and appreciated."));
+        }
+
+        //Компактное решение чтения файла в строку, при помощи библиотеки
+        FileUtils.readFileToString(file, StandardCharsets.UTF_8);
+
+    }
+}
