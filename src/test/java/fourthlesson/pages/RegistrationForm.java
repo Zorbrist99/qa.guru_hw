9c0b5751503e450e5867f9cbf75e890f4b3905cd
Sergey Ermolaev <bertzorbrist@gmail.com>
2024-03-14 21:03:54 +0300

    Убрал всплывающую рекламу на странице

--- src/test/java/fourthlesson/pages/RegistrationForm.java
+++ src/test/java/fourthlesson/pages/RegistrationForm.java
@@ -40,7 +40,7 @@ public class RegistrationForm {
     public RegistrationForm openPage() {
         open("automation-practice-form");
         executeJavaScript("$('.Google-Ad').remove()");
-        executeJavaScript("$('#google_esf').remove()");
+        executeJavaScript("$('.adsbygoogle adsbygoogle-noablate').remove()");
         Attach.screenshotAs( "Скрин");
         return this;
     }
