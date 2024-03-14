04e4ddf24467eff4f65a062485e3146f424c1c6b
Sergey Ermolaev <bertzorbrist@gmail.com>
2024-03-14 17:54:23 +0300

    Добавил команду для запуска сессии через selenoid

--- src/test/java/fourthlesson/TestBase.java
+++ src/test/java/fourthlesson/TestBase.java
@@ -22,6 +22,8 @@ public class TestBase {
         Configuration.browserSize = "1920x1080";
         Configuration.pageLoadStrategy = "eager";
         SelenideLogger.addListener("allure", new AllureSelenide());
+        //Команда запускает сессию не локально, а в Docer контейнере
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
     }
 
     //Если хотим делать один скрин после последнего шага теста
